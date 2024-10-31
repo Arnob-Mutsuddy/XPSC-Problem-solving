13dc5a6ebd26d0573537ac118614ac629770af5c
Arnob-Mutsuddy <165490914+Arnob-Mutsuddy@users.noreply.github.com>
2024-10-31 23:19:54 +0600

    Add files via upload

---
+++ week-01/A - Counting.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+
+    int N = 0;
+    for (int i = A; i <= B; i++)
+    {
+        N++;
+    }
+
+    cout << N << endl;
+
+    return 0;
+}

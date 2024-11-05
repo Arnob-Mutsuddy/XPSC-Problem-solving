de69d503faeddf04df16fa96e2087eca88c8f793
Arnob-Mutsuddy <165490914+Arnob-Mutsuddy@users.noreply.github.com>
2024-11-05 22:20:52 +0600

    Update B_Misha_and_Changing_Handles.cpp

--- week-01/B_Misha_and_Changing_Handles.cpp
+++ week-01/B_Misha_and_Changing_Handles.cpp
@@ -6,37 +6,37 @@ int main()
     int q;
     cin >> q;
 
-    map<string, string> finalHandles;
-    map<string, string> currentHandles;
-    vector<string> originalOrder;
+    map<string, string> fi_han;
+    map<string, string> cur_han;
+    vector<string> order;
 
     for (int i = 0; i < q; i++)
     {
         string old_handle, new_handle;
         cin >> old_handle >> new_handle;
 
-        if (currentHandles.find(old_handle) != currentHandles.end())
+        if (cur_han.find(old_handle) != cur_han.end())
         {
-            // This means old_handle is already associated with an initial handle
-            string initial_handle = currentHandles[old_handle];
-            finalHandles[initial_handle] = new_handle;   // Update final handle
-            currentHandles.erase(old_handle);            // Remove old handle
-            currentHandles[new_handle] = initial_handle; // Map new handle to initial handle
+
+            string initial = cur_han[old_handle];
+            fi_han[initial] = new_handle;
+            cur_han.erase(old_handle);
+            cur_han[new_handle] = initial;
         }
         else
         {
-            // This is a new initial handle (first-time encounter)
-            finalHandles[old_handle] = new_handle;   // Map it to the new handle
-            currentHandles[new_handle] = old_handle; // Associate new handle with this initial one
-            originalOrder.push_back(old_handle);     // Track the initial handle's order
+
+            fi_han[old_handle] = new_handle;
+            cur_han[new_handle] = old_handle;
+            order.push_back(old_handle);
         }
     }
 
-    // Output the result
-    cout << originalOrder.size() << '\n';
-    for (const auto &initial_handle : originalOrder)
+    cout << order.size();
+    cout << '\n';
+    for (const auto &initial : order)
     {
-        cout << initial_handle << " " << finalHandles[initial_handle] << '\n';
+        cout << initial << " " << fi_han[initial] << '\n';
     }
 
     return 0;

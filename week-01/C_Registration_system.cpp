5948c878d680b90ddb963b84e5841a1610ffff13
Arnob-Mutsuddy <165490914+Arnob-Mutsuddy@users.noreply.github.com>
2024-11-09 12:16:24 +0600

    Add files via upload

---
+++ week-01/C_Registration_system.cpp
@@ -0,0 +1,32 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    map<string, int> mp;
+
+    for (int i = 0; i < n; i++)
+    {
+        string name;
+        cin >> name;
+
+        if (mp[name] == 0)
+        {
+            cout << "OK" << endl;
+            mp[name] = 1;
+        }
+        else
+        {
+            cout << name << mp[name] << endl;
+            mp[name]++;
+        }
+    }
+
+    return 0;
+}
